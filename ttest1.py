5836f3cf0a18089546fd5648047d37b259ead0b2
Assahin <94584246+Assahin@users.noreply.github.com>
2022-01-13 09:41:12 +0700

    Add files via upload

--- ttest1.py
+++ ttest1.py
@@ -4,24 +4,32 @@ y=0
 hp=0
 print('первая комната. В комнате 4 двери с каждой стороны')
 dej1='взять компас'
-keyy=random.randint(1,5)
-keyx=random.randint(1,5)
-exy=random.randint(4,5)
-exx=random.randint(4,5)
+keyy=random.randint(-1,1)
+keyx=random.randint(-1,1)
+exy=random.randint(-1,1)
+exx=random.randint(-1,1)
+key=0
 use=''
-while hp==0:
+while True:
+    if keyx==x and keyy==y:
+        key=1
+        print("у вас есть ключ")
+    if exx==x and exy==y:
+        print("В этой комнате выход")
+        if key==1:
+            break
     while x==0 and y==0:
         dej2='пойти в другую комнату'
-        dej=input('введити что хотите сделать. Возможные действия:'+dej1+', ' +dej2+', '+use)
+        dej=input('введити что хотите сделать. Возможные действия:'+dej1+', ' +dej2+use)
         if dej=='чит':
             print(keyx,';',keyy,' ', exx,';', exy)
         elif dej==dej1:
-            use='использовать компас'
+            use=', использовать компас'
             dej1=''
         elif dej==use:
             print('координаты:',x,';',y)
         elif dej==dej2 and dej!='':
-            di=int(input('в какую дверь войти. Север, Юг, Восток, Запад'))
+            di=input('в какую дверь войти. Север, Юг, Восток, Запад')
             if di=='Север':
                 y=1
             elif di=='Восток':
@@ -31,7 +39,63 @@ while hp==0:
             elif di=='Запад':
                 x=-1
     while x==1 and y==0:
-        a=random.randint(1,4)
+        a=random.randint(1,3)
+        if a==1:
+            dej1="вернуться обратно"
         print('в этой комнате ', a,' дверей, учитывая ту, из которой вы вышли.')
-        dej=input('введити что хотите сделать. Возможные действия:'+dej1+', ' +dej2+', '+use)
-   
+        dej=input('введити что хотите сделать. Возможные действия:'+dej1+', ' +dej2+use)
+        if dej==dej1:
+            if a==1:
+                x=0
+            if a==2:
+                di=input("Вы можете пойти обратно, а можете пойти на Север")
+                if di=="обратно":
+                    x=0
+                elif di =="Север":
+                    y=1
+            if a==3:
+                di=input("Вы можете пойти обратно, а можете пойти на Север и на Юг")
+                if di=="обратно":
+                    x=0
+                elif di =="Север":
+                    y=1
+                elif di =="Юг":
+                    y=-1
+    while x==1 and y==1:
+        a=random.randint(1,2)
+        if a==1:
+            dej1="вернуться обратно"
+        print('в этой комнате ', a,' дверей, учитывая ту, из которой вы вышли.')
+        dej=input('введити что хотите сделать. Возможные действия:'+dej1+', ' +dej2+use)
+        if dej==dej1:
+            if a==1:
+                y=0
+            if a==2:
+                di=input("Вы можете пойти обратно, а можете пойти на Запад")
+                if di=="обратно":
+                    y=0
+                elif di =="Запад":
+                    x=1
+    while x=0 and y=1:
+        a=random.randint(1,3)
+        if a==1:
+            dej1="вернуться обратно"
+        print('в этой комнате ', a,' дверей, учитывая ту, из которой вы вышли.')
+        dej=input('введити что хотите сделать. Возможные действия:'+dej1+', ' +dej2+use)
+        if dej==dej1:
+            if a==1:
+                x=1
+            if a==2:
+                di=input("Вы можете пойти обратно, а можете пойти на Запад")
+                if di=="обратно":
+                    x=1
+                elif di =="Запад":
+                    x=-1
+            if a==3:
+                di=input("Вы можете пойти обратно, а можете пойти на Запад и на Юг")
+                if di=="обратно":
+                    x=1
+                elif di =="Запад":
+                    x=-1
+                elif di =="Юг":
+                    y=0
